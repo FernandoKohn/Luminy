f299208eef3fa7348fd48c4c9a1ba537f3344a61
FernandoKohn <contatofernandokohn@gmail.com>
2024-04-19 21:02:52 -0300

    Pagina de login

--- projeto/src/components/pages/Login.tsx
+++ projeto/src/components/pages/Login.tsx
@@ -1,39 +1,75 @@
-import { useState } from "react"
+import { ReactElement, useState } from "react"
 import styles from "./Login.module.css"
-import { Link, useLocation } from "react-router-dom"
+import { Link } from "react-router-dom"
+
 
 
 export const Login = () => {
 
-    const [usuario, setUsuario] = useState<object>({})
+    const [usuario, setUsuario] = useState<object>([])
+    const [error, setError] = useState<string>()
 
-   let location = useLocation()
+    console.log(usuario)
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setUsuario({...usuario, [e.target.name]: e.target.value })
+    const handleSubmit = (e: any): any => {
+        e.preventDefault()
+        let data = new FormData(e.target)
+        let value = Object.fromEntries(data.entries())
+        
+        if (Object.values(usuario).indexOf(value) !== undefined) {
+            setError("Usuário já cadastrado")
+        } else {
+            fetch("https://boatneck-mulberry-chime.glitch.me/user", {
+                method: "POST",
+                headers: {
+                    "Content-Type":"application/json"
+                },
+                body: JSON.stringify(value)
+            })
+            .then(resp => resp.json())
+            .catch(err => console.log(err))
+        }
+
+    }
+
+    const resetdb = (): any => {
+        fetch(`https://boatneck-mulberry-chime.glitch.me/user/1`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            },
+        })
+            .then(resp => resp.json())
+            .catch(err => console.log(err))
     }
 
     return (
         <div className={styles.main}>
-           
-                <form className={styles.formContainer}>
-                    <h1>SIGN IN</h1>
-                    <p> COMECE A APROVEITAR OS BENEFÍCIOS HOJE MESMO!</p>
-                    <label htmlFor="user">USUÁRIO</label>
-                    <input type="text" id="user" maxLength={25} onChange={handleChange} required/>
-                    <label htmlFor="user">SENHA</label>
-                    <input type="password" id="password" maxLength={25} onChange={handleChange} required/>
-                    <Link to="/ResetPassword">
-                        <p id={styles.forgotPassword}>ESQUECEU A SENHA?</p>
-                    </Link>
-                    <button type="submit">ENTRAR</button>
-                </form>
-                <div>
-                    <h1>NOVO AQUI?</h1>
-                    <Link to={"/Cadastro"}>
-                        <h1>CADASTRE-SE</h1>
-                    </Link>
-                </div>
+
+            <form className={styles.formContainer} onSubmit={handleSubmit}>
+                <h1>SIGN IN</h1>
+                <p> COMECE A APROVEITAR OS BENEFÍCIOS HOJE MESMO!</p>
+                <label htmlFor="user" >USUÁRIO</label>
+                <input type="text" id="user" name="user" maxLength={25} required />
+                <label htmlFor="user">SENHA</label>
+                <input type="password" id="password" name="password" minLength={6} maxLength={12} required />
+                <Link to="/ResetPassword">
+                    <p id={styles.forgotPassword}>ESQUECEU A SENHA?</p>
+                </Link>
+                <button type="submit">ENTRAR</button>
+                {error && (
+                    error
+                )}
+            </form>
+            <div>
+                <h1>NOVO AQUI?</h1>
+                <Link to={"/Cadastro"}>
+                    <h1>CADASTRE-SE</h1>
+                </Link>
+            </div>
+            <div className={styles.delete} onClick={resetdb}>
+                <i className='bx bxl-typescript'></i>
+            </div>
         </div>
     )
 }
